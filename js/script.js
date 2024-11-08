518319a519e767ef6a1301a65fe2ab8f627d87a9
carmenguarino <carm.guarino@gmail.com>
2024-11-08 08:53:21 +0100

    carm: conf final day

--- js/script.js
+++ js/script.js
@@ -1030,8 +1030,8 @@ function viz(){
 	  var m = 0;
 	  var s = 0;
 
-	  //var inc = 288/10;
-	  var inc = 96/10;
+	  var inc = 288/10;
+	  //var inc = 96/10;
 
 	  var ph = 0;
 	  var pm = 0;
@@ -1123,8 +1123,8 @@ function viz(){
 	    timeelapsed = 0;
 	    
 
-	   startdatetime = luxon.DateTime.fromISO(ritualdata.ritual.starttime).minus({day: 1});
-	   enddatetime = luxon.DateTime.fromISO(ritualdata.ritual.starttime);
+	   //startdatetime = luxon.DateTime.fromISO(ritualdata.ritual.starttime).minus({day: 1});
+	   //enddatetime = luxon.DateTime.fromISO(ritualdata.ritual.starttime);
 
 	   // if(islastday){
 	   //	startdatetime = luxon.DateTime.fromISO(ritualdata.ritual.starttime).minus({day: parseInt( ritualdata.howmanydaysdataonlastday)  });
@@ -1133,8 +1133,8 @@ function viz(){
 	   //	inc = 120 * (ritualdata.howmanydaysdataonlastday - ritualdata.howmanydaysdatatoonlastday );
 	   // }
 	    
-	   //startdatetime = luxon.DateTime.fromISO("2024-10-28T20:00:00");
-	   //enddatetime = luxon.DateTime.fromISO("2024-10-30T20:00:00");
+	   startdatetime = luxon.DateTime.fromISO("2024-10-28T20:00:00");
+	   enddatetime = luxon.DateTime.fromISO("2024-10-30T20:00:00");
 	    
 	   st = startdatetime;
 
@@ -1554,8 +1554,8 @@ function endRitual(){
 		);
 
 	// andare all'assemblea
-	toAssembly();
-	//toCouples();
+	//toAssembly();
+	toCouples();
 	//toMenu();
 }
 
